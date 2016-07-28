56a7c245fdca7b38559d0e546ac46039001041d4
rickharris-dev <rickharris724@gmail.com>
2016-07-27 17:53:19 -0700

    Final organization

---
+++ chef_is_fun/cookbooks/holberton-wrapper/attributes/default.rb
@@ -0,0 +1,3 @@
+# Configure OpenSSH
+default['openssh']['client']['password_authentication'] = 'no'
+default['openssh']['server']['permit_root_login'] = 'no'
--- chef_is_fun/cookbooks/holberton-wrapper/recipes/default.rb
+++ chef_is_fun/cookbooks/holberton-wrapper/recipes/default.rb
@@ -8,14 +8,28 @@
 #
 
 # Add the dotdeb repo
+include_recipe 'openssh'
+
 apt_repository 'dotdeb' do
   uri 'http://packages.dotdeb.org'
   components ['main']
 end
 
-# Configure Open SSH
-node.default['openssh']['server']['permit_root_login'] = 'no'
-node.default['openssh']['server']['password_authentication'] = 'no'
+# Ensure OpenSSH directory exists
+directory '/var/run/sshd' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+# Recognize OpenSSH
+begin
+  r = resources(service: 'ssh')
+  if 'debian' == node['platform']
+    r.provider = Chef::Provider::Service::Debian
+  end
+end
 
 # Set user accounts
 user_account 'holberton' do
@@ -23,28 +37,13 @@ user_account 'holberton' do
   comment 'Holberton School'
 end
 
-user_account 'rick' do
-  ssh_keys 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5hsn/3vgN5aALc5KRHmKQ6DriypjBuFVD4D3GqIAX7GzCxANCuWr8GNOIQz2saIIlQjxUrVay4jnyS/1Fk4YqmXQe5oLYXjwMFRaZhwj9S4npNauEKrMh28H8C4dqKPnyqUK7Pd2BFuAvgM1g0el1jYvqAY5XlnGPZFDnoanSqgUPeO+eSuoUxlInAsgHA2HlYDlK4B7WBxqiRF5P0wYLGeHZKujb0miXNwGPa9v/EhBKrvciiTyUNnPC9fqeHeBiLG87I/7tqDNfZcqlm4s6yNeuBPlv2Ww1r+0/WPAr7XSORlR8GkTIWahmWEgy8mItpOwnS8EKGdLL//a7qYErj9R0RxvO9I5AKEXPk75XTNGc4Kv12NUaR/jEDtk0/3NSmstmvq95AE7xSPpUIX9F3pc12Xu/zrmWRH9PmipxAH9BOEDqTHQbn4Dbtgm97U4yuJtGdBWjVJUxTaEquMofE8ffFasmzeayRdzNc/0lqXDV1kordIfjzAX4FSB4GjaCMnURO6ms6Mvw85wQAxnw3UyPkjsfOEcAKG3q3bF2+qiDZxPuaI9PSIkjSezs0AO7Y2duy3jf+fyONBaQ8OO8rT0w2fm4s8CgMZFQwmjWBH4kKVmHT8wsI1l+ZkEUXWOsb90/c5iZ0x4yHoZRYsa0xxi+rImvWpk+MfXkXI3Scw== rickharris724@gmail.com'
-  comment 'Rick'
-end
-
 #Install and configure nginx
 package 'nginx'
 
 service 'nginx' do
   action [:enable, :start]
-end
-
-# Change homepage to hello world template
-file '/var/www/html/index.html' do
-  content '<html>
-  <body>
-    <h1>hello world</h1>
-  </body>
-</html>'
+  provider Chef::Provider::Service::Debian
 end
 
 # Install net-tools
 package 'net-tools'
-
-# Configure firewall
